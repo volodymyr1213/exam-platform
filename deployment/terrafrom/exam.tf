6a91f0504f4481c9f4b6f8efa7d15b18ad1f6c91
daudmu21 <daudmuhudin@gmail.com>
2019-10-10 14:12:05 -0500

    edit exam.tf

--- deployment/terrafrom/exam.tf
+++ deployment/terrafrom/exam.tf
@@ -4,9 +4,5 @@ resource "helm_release" "exam-platform" {
   name       = "fuchicorp-exam"
   namespace = "${var.namespace}"
   chart = "./Chart.yaml"
- }
-
- values = [
-    "${data.template_file.exam_values.rendered}"
-  ]
 }
+ }
