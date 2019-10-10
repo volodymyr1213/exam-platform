fff1810609509cfcf3f770ce1361eaae6d95407b
daudmu21 <daudmuhudin@gmail.com>
2019-10-10 14:13:12 -0500

    edit

--- deployment/terrafrom/exam.tf
+++ deployment/terrafrom/exam.tf
@@ -1,8 +1,5 @@
-data "template_file" "exam_values" {
-  template = "${file("/Chart.yaml")}"
 resource "helm_release" "exam-platform" {
   name       = "fuchicorp-exam"
   namespace = "${var.namespace}"
   chart = "./Chart.yaml"
 }
- }
