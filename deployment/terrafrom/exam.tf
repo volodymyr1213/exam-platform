8426b12c79f4f7033182f2bc37674570dfb502e1
daudmu21 <daudmuhudin@gmail.com>
2019-10-10 13:58:34 -0500

    edit values and exam.tf

--- deployment/terrafrom/exam.tf
+++ deployment/terrafrom/exam.tf
@@ -1,5 +1,18 @@
+data "template_file" "jenkins_values" {
+  template = "${file("/Chart.yaml")}"
+
+resource "local_file" "jenkins_helm_chart_values" {
+  content  = "${trimspace(data.template_file.exam_values.rendered)}"
+  filename = "/.cache/exam_values.yaml"
+}
+
 resource "helm_release" "exam-platform" {
   name       = "fuchicorp-exam"
   namespace = "${var.namespace}"
   chart = "./Chart.yaml"
-  }
+ }
+
+ values = [
+    "${data.template_file.exam_values.rendered}"
+  ]
+}
