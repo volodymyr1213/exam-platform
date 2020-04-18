901ea17b5d24df366a9cda48e50789979807df21
bkarakas <bkarakasu@gmail.com>
2020-04-18 18:58:26 -0500

    deployment name changed

--- deployments/terraform/exam-platform.tf
+++ deployments/terraform/exam-platform.tf
@@ -1,6 +1,6 @@
-module "exam-platform_deploy" {
+module "exam-platform-deploy" {
   source  = "fuchicorp/chart/helm"
-  deployment_name        = "exam-platform-deployment"
+  deployment_name        = "exam-platform"
   deployment_environment = "${var.deployment_environment}"
   deployment_endpoint    = "${lookup(var.deployment_endpoint, "${var.deployment_environment}")}"
   deployment_path        = "exam-platform"
