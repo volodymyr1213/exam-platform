37171cbc8b499286c029ff405b0fb030f5928ea3
bkarakas <bkarakasu@gmail.com>
2020-04-18 20:24:36 -0500

    '' added

--- deployments/terraform/exam-platform.tf
+++ deployments/terraform/exam-platform.tf
@@ -1,5 +1,5 @@
 module "exam-platform-deploy" {
-  source  = "fuchicorp/chart/helm"
+  source                 = "fuchicorp/chart/helm"
   deployment_name        = "exam-platform"
   deployment_environment = "${var.deployment_environment}"
   deployment_endpoint    = "${lookup(var.deployment_endpoint, "${var.deployment_environment}")}"
